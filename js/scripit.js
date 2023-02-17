bccce13d489474f77b994e2fc396d7d8354a491b
Brunonavarrooficial <bnacessoria@gmail.com>
2023-02-17 00:23:34 -0300

    no

--- js/scripit.js
+++ js/scripit.js
@@ -37,7 +37,7 @@ const getWeatherData = async (city) => {
 
     const res = await fetch(apiWeatherURL, {
         headers: {
-            mode: 'cors', // no-cors, *cors, same-origin
+            mode: 'no-cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
             headers: {
