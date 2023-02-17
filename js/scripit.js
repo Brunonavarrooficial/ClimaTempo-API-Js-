7b4c4fac117731c493469cf7882e207f0540264c
Brunonavarrooficial <bnacessoria@gmail.com>
2023-02-17 01:24:54 -0300

    t

--- js/scripit.js
+++ js/scripit.js
@@ -42,7 +42,6 @@ const getWeatherData = async (city) => {
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
             headers: {
-                'Content-Type': 'application/json',
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             redirect: 'follow', // manual, *follow, error
