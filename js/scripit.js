5b5d12fb6e0c10c50e4136a8220f7f057b69b281
Brunonavarrooficial <bnacessoria@gmail.com>
2023-02-17 00:21:40 -0300

    teset fetch

--- js/scripit.js
+++ js/scripit.js
@@ -45,7 +45,7 @@ const getWeatherData = async (city) => {
                 // 'Content-Type': 'application/x-www-form-urlencoded',
             },
             redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            referrerPolicy: 'strict-origin-when-cross-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         }
     });
     const data = await res.json();
